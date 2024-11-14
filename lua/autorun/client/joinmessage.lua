c7a254a620451bed1d3e280fb883917327ca0d28
Pancakes <patapancakes@pagefault.games>
2024-11-13 22:46:26 -0500

    Tweak join message, use shorter one after first time

--- lua/autorun/client/joinmessage.lua
+++ lua/autorun/client/joinmessage.lua
@@ -1,9 +1,14 @@
 hook.Add("Initialize", "CloudboxJoinMessage", function()
 	chat.AddText(Color(255, 255, 255), "This server is running ", Color(184, 227, 255), "Cloudbox", Color(255, 255, 255),", the new Toybox!")
 	chat.AddText(Color(255, 255, 255), "Use the ", Color(184, 227, 255), "Cloudbox", Color(255, 255, 255)," tab in the spawn menu to spawn things.")
+
+	if file.Exists("cloudbox/seen_full_message.txt", "DATA") then return end
+
 	chat.AddText("")
-	chat.AddText(Color(255, 255, 255), "Thousands of original Toybox uploads are available for you to use.")
+	chat.AddText(Color(255, 255, 255), "Over a thousand original Toybox uploads are available for you to use.")
 	chat.AddText(Color(255, 255, 255), "Compatibility is still being improved, so please expect bugs.")
 	chat.AddText("")
-	chat.AddText(Color(255, 255, 255), "Have any questions or concerns? DM me on Discord at @keroronpa")
+	chat.AddText(Color(255, 255, 255), "Check us out on the workshop for more info!")
+
+	file.Write("cloudbox/seen_full_message.txt", "")
 end)
