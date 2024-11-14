9bfb220b279ab0e600ee675ddd4aae10059e1a2b
Pancakes <patapancakes@pagefault.games>
2024-11-14 11:36:33 -0500

    Refactor compatibility.lua

--- lua/autorun/compatibility.lua
+++ lua/autorun/compatibility.lua
@@ -16,12 +16,37 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+local cEntity = FindMetaTable("Entity")
+local cIMaterial = FindMetaTable("IMaterial")
+local cPlayer = FindMetaTable("Player")
+
 // "KeyValuesToTable and TableToKeyValues are now in the util library"
 KeyValuesToTable = util.KeyValuesToTable
 TableToKeyValues = util.TableToKeyValues
 
 // "SetMaterialOverride, cam.StartMaterialOverride (which did the same thing) are removed and replaced with render.MaterialOverride"
-if CLIENT then SetMaterialOverride = render.MaterialOverride end
+if CLIENT then
+	SetMaterialOverride = render.MaterialOverride
+	cam.StartMaterialOverride = render.MaterialOverride
+end
+
+// "IMaterial:SetMaterialTexture is now IMaterial:SetTexture"
+cIMaterial.GetMaterialTexture = cIMaterial.GetTexture
+cIMaterial.SetMaterialTexture = cIMaterial.SetTexture
+
+// "utilx no longer exists. Please replace all ocurances of utilx in your code to the util"
+utilx = util
+
+// "timer.IsTimer is now timer.Exists"
+timer.IsTimer = timer.Exists
+
+// "math duplicate functions removed (Deg2Rad, Rad2Deg)"
+math.Deg2Rad = math.rad
+math.Rad2Deg = math.deg
+
+// "Renamed Material:[Get|Set]Material to Material:[Get|Set]** (ie, :SetVector instead of :SetMaterialVector)"
+cIMaterial.GetMaterialVector = cIMaterial.GetVector
+cIMaterial.SetMaterialVector = cIMaterial.SetVector
 
 // "Moved some functions into the game"
 isDedicatedServer = game.IsDedicated
@@ -31,17 +56,48 @@ MaxPlayers = game.MaxPlayers
 // "WorldSound has been replaced with sound.Play"
 WorldSound = sound.Play
 
+// "Player:GetCursorAimVector is now Player:GetAimVector"
+cPlayer.GetCursorAimVector = cPlayer.GetAimVector
+cPlayer.GetPlayerAimVector = cPlayer.GetAimVector
+
 // "ValidEntity has been removed. Use IsValid instead"
 ValidEntity = IsValid
 
+// mathx is now math
+mathx = math
+
+function cEntity:SetColorCloudbox(r, g, b, a)
+	if IsColor(r) then
+		self:SetColor(r)
+	else
+		self:SetColor(Color(r, g, b, a))
+	end
+end
+
+function cEntity:SetModelScaleCloudbox(vector)
+	self:SetLegacyTransform(true)
+
+	local x, y, z = vector:Unpack()
+
+	self:SetModelScale((x + y + z) / 3)
+end
+
+function CloudboxCreateFont(font_name, size, weight, antialiasing, additive, new_font_name, drop_shadow, outlined, blur)
+	surface.CreateFont(new_font_name, {
+		font = font_name,
+		size = size,
+		weight = weight,
+		antialias = antialiasing,
+		additive = additive,
+		shadow = drop_shadow,
+		outline = outlined,
+		blur = blursize
+	})
+end
+
 function gm13ize(script)
-	local ent = FindMetaTable("Entity")
-
-	// "IMaterial:SetMaterialTexture is now IMaterial:SetTexture"
-	local translated, _ = string.gsub(script, ":SetMaterialTexture%(", ":SetTexture%(")
-
-	// "utilx no longer exists. Please replace all ocurances of utilx in your code to the util"
-	translated, _ = string.gsub(translated, "utilx.", "util.")
+	// "Entity:SetColor and Entity:GetColor now deal with Colors only"
+	local translated, _ = string.gsub(script, ":SetColor%(", ":SetColorCloudbox%(")
 
 	// "entity.Classname. Caps is now enforced properly. Use entity.ClassName instead. (N is upper case)"
 	translated, _ = string.gsub(translated, ".Classname", ".ClassName")
@@ -53,63 +109,11 @@ function gm13ize(script)
 	translated, _ = string.gsub(translated, ":SetAngle%(", ":SetAngles%(")
 	translated, _ = string.gsub(translated, ":GetAngle%(", ":GetAngles%(")
 
-	// "timer.IsTimer is now timer.Exists"
-	translated, _ = string.gsub(translated, "timer.IsTimer%(", "timer.Exists%(")
-
-	// "math duplicate functions removed (Deg2Rad, Rad2Deg)"
-	translated, _ = string.gsub(translated, "math.Deg2Rad%(", "math.rad%(")
-	translated, _ = string.gsub(translated, "math.Rad2Deg%(", "math.deg%(")
-
-	// "Renamed Material:[Get|Set]Material to Material:[Get|Set]** (ie, :SetVector instead of :SetMaterialVector)"
-	translated, _ = string.gsub(translated, ":GetMaterialVector%(", ":GetVector%(")
-	translated, _ = string.gsub(translated, ":SetMaterialVector%(", ":SetVector%(")
-
-	// "Player:GetCursorAimVector is now Player:GetAimVector"
-	translated, _ = string.gsub(translated, ":GetCursorAimVector%(", ":GetAimVector%(")
-	translated, _ = string.gsub(translated, ":GetPlayerAimVector%(", ":GetAimVector%(")
-
-	// Change mathx to math
-	translated, _ = string.gsub(translated, "mathx.", "math.")
-
 	// SetModelScale takes number now, not Vector
-	function ent:SetModelScaleCloudbox(vector)
-		self:SetLegacyTransform(true)
-
-		local x, y, z = vector:Unpack()
-
-		self:SetModelScale((x + y + z) / 3)
-	end
-
 	translated, _ = string.gsub(translated, ":SetModelScale%(", ":SetModelScaleCloudbox%(")
 
-	// SetColor takes Color() now
-	function ent:SetColorCloudbox(r, g, b, a)
-		if IsColor(r) then
-			self:SetColor(r)
-		else
-			self:SetColor(Color(r, g, b, a))
-		end
-	end
-
-	translated, _ = string.gsub(translated, ":SetColor%(", ":SetColorCloudbox%(")
-
 	// CreateFont takes a table now
-	function CloudboxCreateFont(font_name, size, weight, antialiasing, additive, new_font_name, drop_shadow, outlined, blur)
-		surface.CreateFont(new_font_name, {
-			font = font_name,
-			size = size,
-			weight = weight,
-			antialias = antialiasing,
-			additive = additive,
-			shadow = drop_shadow,
-			outline = outlined,
-			blur = blursize
-		})
-	end
-
 	translated, _ = string.gsub(translated, "surface.CreateFont%(", "CloudboxCreateFont%(")
 
 	return translated
 end
-
-
