9c74ffebcae05ae84538f3fc42db6a2f2a14daca
Pancakes <patapancakes@pagefault.games>
2024-11-15 12:21:26 -0500

    Allow users to disable Cloudbox join message

--- lua/autorun/client/joinmessage.lua
+++ lua/autorun/client/joinmessage.lua
@@ -1,4 +1,6 @@
 hook.Add("Initialize", "CloudboxJoinMessage", function()
+	if !GetConVar("cloudbox_showjoinmessage"):GetBool() then return end
+
 	chat.AddText(Color(255, 255, 255), "This server is running ", Color(184, 227, 255), "Cloudbox", Color(255, 255, 255),", the new Toybox!")
 	chat.AddText(Color(255, 255, 255), "Use the ", Color(184, 227, 255), "Cloudbox", Color(255, 255, 255)," tab in the spawn menu to spawn things.")
 
@@ -12,3 +14,5 @@ hook.Add("Initialize", "CloudboxJoinMessage", function()
 
 	file.Write("cloudbox/seen_full_message.txt", "")
 end)
+
+CreateConVar("cloudbox_showjoinmessage", "1", FCVAR_ARCHIVE, "Show Cloudbox chat message on join", 0, 1)
